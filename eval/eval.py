c7473b263613c9cb2bd5c1097510ce76fd8a5250
farodey <farodey@yandex.ru>
2022-07-10 11:46:38 +0300

    реализован и протестирован оценщик eval_7_hand

--- eval/eval.py
+++ eval/eval.py
@@ -43,39 +43,60 @@ def eval_7_hand(cards, n_cards=7):
         if retval and n_dups < 3:
             return retval
 
-        if n_dups == 0:
-            # Старшая карта
-            return hand_type_value(HAND_TYPE_NOPAIR) + topFiveCardsTable[ranks]
-        elif n_dups == 1:
-            # Пара
-            two_mask = ranks ^ (sc ^ sd ^ sh ^ ss)
-            retval = hand_type_value(HAND_TYPE_ONEPAIR) + top_card_value(topCardTable[two_mask])
+    if n_dups == 0:
+        # Старшая карта
+        return hand_type_value(HAND_TYPE_NOPAIR) + topFiveCardsTable[ranks]
+    elif n_dups == 1:
+        # Пара
+        two_mask = ranks ^ (sc ^ sd ^ sh ^ ss)
+        retval = hand_type_value(HAND_TYPE_ONEPAIR) + top_card_value(topCardTable[two_mask])
+        t = ranks ^ two_mask
+        kickers = (topFiveCardsTable[t] >> CARD_WIDTH) & 0x0000000F
+        retval += kickers
+        return retval
+    elif n_dups == 2:
+        # Две пары или тройка
+        two_mask = ranks ^ (sc ^ sd ^ sh ^ ss)
+        if two_mask:
             t = ranks ^ two_mask
-            kickers = (topFiveCardsTable[t] >> CARD_WIDTH) & 0x0000000F
-            retval += kickers
+            retval = hand_type_value(HAND_TYPE_TWOPAIR) + (topFiveCardsTable[two_mask]
+                                & (TOP_CARD_MASK | SECOND_CARD_MASK) + third_card_value(topCardTable[t]))
             return retval
-        elif n_dups == 2:
-            # Две пары или тройка
-            two_mask = ranks ^ (sc ^ sd ^ sh ^ ss)
-            if two_mask:
-                t = ranks ^ two_mask
-                retval = hand_type_value(HAND_TYPE_TWOPAIR) + topFiveCardsTable[two_mask] \
-                    & (TOP_CARD_MASK | SECOND_CARD_MASK) + third_card_value(topCardTable[t])
-                return retval
-            else:
-                # Тройка. В three_mask устанавливаем бит карты из которой состоит тройка
-                three_mask = ((sc & sd) | (sh & ss)) & ((sc & sh) | (sd & ss))
-                retval = hand_type_value(HAND_TYPE_TRIPS) + top_card_value(topCardTable[three_mask])
-                t = ranks ^ three_mask  # Выключаем ранг тройки в маске
-                second = topCardTable[t]
-                retval += second_card_value(second)
-                t ^= (1 << second)      # Выключаем ранг кикера
-                retval += third_card_value(topCardTable[t])
-                return retval
         else:
-            four_mask = sh & sd & sc & ss
-            if four_mask:
-                tc = topCardTable[four_mask]
-                retval = hand_type_value(HAND_TYPE_QUADS) + top_card_value(tc) + \
-                    second_card_value(topCardTable[ranks ^ (1 << tc)])
-                return retval
+            # Тройка. В three_mask устанавливаем бит карты из которой состоит тройка
+            three_mask = ((sc & sd) | (sh & ss)) & ((sc & sh) | (sd & ss))
+            retval = hand_type_value(HAND_TYPE_TRIPS) + top_card_value(topCardTable[three_mask])
+            t = ranks ^ three_mask  # Выключаем ранг тройки в маске
+            second = topCardTable[t]
+            retval += second_card_value(second)
+            t ^= (1 << second)      # Выключаем ранг кикера
+            retval += third_card_value(topCardTable[t])
+            return retval
+    else:
+        four_mask = sh & sd & sc & ss
+        if four_mask:
+            tc = topCardTable[four_mask]
+            retval = hand_type_value(HAND_TYPE_QUADS) + top_card_value(tc) + \
+                second_card_value(topCardTable[ranks ^ (1 << tc)])
+            return retval
+        two_mask = ranks ^ (sc ^ sd ^ sh ^ ss)
+        if nBitsTable[two_mask] != n_dups:
+            # Фулл-Хаус
+            three_mask = ((sc & sd) | (sh & ss)) & ((sc & sh) | (sd & ss))
+            retval = hand_type_value(HAND_TYPE_FULLHOUSE)
+            tc = topCardTable[three_mask]
+            retval += top_card_value(tc)
+            t = (two_mask | three_mask) ^ (1 << tc)
+            retval += second_card_value(topCardTable[t])
+            return retval
+        if retval:
+            return retval
+        else:
+            # Две пары
+            retval = hand_type_value(HAND_TYPE_TWOPAIR)
+            top = topCardTable[two_mask]
+            retval += top_card_value(top)
+            second = topCardTable[two_mask ^ (1 << top)]
+            retval += second_card_value(second)
+            retval += third_card_value(topCardTable[ranks ^ (1 << top) ^ (1 << second)])
+            return retval
--- test/test.py
+++ test/test.py
@@ -1,34 +1,11 @@
-from eval.deck import string_to_card, text_to_mask
+from eval.deck import string_to_card, text_to_mask, get_mask
 from eval.eval import eval_7_hand
+from eval.handval import *
+import time
 
 
 def test_string_to_card():
     print(string_to_card('2h'))
-    print(string_to_card('3h'))
-    print(string_to_card('4h'))
-    print(string_to_card('5h'))
-    print(string_to_card('6h'))
-    print(string_to_card('7h'))
-    print(string_to_card('8h'))
-    print(string_to_card('9h'))
-    print(string_to_card('Th'))
-    print(string_to_card('Jh'))
-    print(string_to_card('Qh'))
-    print(string_to_card('Kh'))
-    print(string_to_card('Ah'))
-    print(string_to_card('2d'))
-    print(string_to_card('3d'))
-    print(string_to_card('4d'))
-    print(string_to_card('5d'))
-    print(string_to_card('6d'))
-    print(string_to_card('7d'))
-    print(string_to_card('8d'))
-    print(string_to_card('9d'))
-    print(string_to_card('Td'))
-    print(string_to_card('Jd'))
-    print(string_to_card('Qd'))
-    print(string_to_card('Kd'))
-    print(string_to_card('Ad'))
 
 
 def test_text_to_mask():
@@ -36,10 +13,63 @@ def test_text_to_mask():
 
 
 def test_eval_7_hand():
-    print(eval_7_hand(text_to_mask('AcKcQcJcTc2s5h')))
+    print(eval_7_hand(text_to_mask('2h3h4h5h2d3d7d')))
+
+
+def enum_test_eval_7_hand():
+    sf = 0
+    q = 0
+    fh = 0
+    f = 0
+    st = 0
+    t = 0
+    two = 0
+    pair = 0
+    nopair = 0
+
+    start_time = time.time()
+    for c1 in range(52):
+        for c2 in range(c1 + 1, 52):
+            for c3 in range(c2 + 1, 52):
+                for c4 in range(c3 + 1, 52):
+                    for c5 in range(c4 + 1, 52):
+                        for c6 in range(c5 + 1, 52):
+                            for c7 in range(c6 + 1, 52):
+                                ret = eval_7_hand(get_mask(c1) | get_mask(c2) | get_mask(c3) | get_mask(c4) | get_mask(c5) | get_mask(c6) | get_mask(c7))
+                                r = ret >> HAND_TYPE_SHIFT
+                                if r == HAND_TYPE_STFLUSH:
+                                    sf += 1
+                                elif r == HAND_TYPE_QUADS:
+                                    q += 1
+                                elif r == HAND_TYPE_FULLHOUSE:
+                                    fh += 1
+                                elif r == HAND_TYPE_FLUSH:
+                                    f += 1
+                                elif r == HAND_TYPE_STRAIGHT:
+                                    st += 1
+                                elif r == HAND_TYPE_TRIPS:
+                                    t += 1
+                                elif r == HAND_TYPE_TWOPAIR:
+                                    two += 1
+                                elif r == HAND_TYPE_ONEPAIR:
+                                    pair += 1
+                                elif r == HAND_TYPE_NOPAIR:
+                                    nopair += 1
+                                else:
+                                    print(c1, c2, c3, c4, c5, c6, c7)
+
+    print("--- %s seconds ---" % (time.time() - start_time))
+
+    print(nopair)
+    print(pair)
+    print(two)
+    print(t)
+    print(st)
+    print(f)
+    print(fh)
+    print(q)
+    print(sf)
 
 
 if __name__ == '__main__':
-    # test_string_to_card()
-    #test_text_to_mask()
-    test_eval_7_hand()
+    enum_test_eval_7_hand()
